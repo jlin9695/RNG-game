8382279206dd979198a9adf260c1f1ae3e8fd9cc
Josh Lin <jlin9695@gmail.com>
2020-03-25 00:41:33 -0500

    Update RNG game with final set of code

--- Random number generator project.py
+++ Random number generator project.py
@@ -1,3 +1,4 @@
+"""
 import random
 
 def guessing_game(g):
@@ -31,3 +32,65 @@ while guess != number:
     except ValueError:
         print("Warning! What you typed in was either not a number or contained things that were not numbers. Please make sure you are answering properly with numbers.")
         guess = int(input("Please guess a number between 0 and 20: "))
+        
+This was the first try. The set of code below is the final draft.
+"""
+import random
+
+#def randomfunction(g):
+    #return int(g)
+def num_check(g):
+    try:
+        return int(g)
+    except ValueError:
+        print("Value Error, please try again!")
+        return input("Please enter a number this time, not a character or string of words: ")
+
+def guessing_game(g):
+    #try:
+        #int(g)
+    #except ValueError:
+        #print("Warning! What you typed in was either not a number or contained things that were not numbers. Please make sure you are answering properly with numbers.")
+        #g = input("Please guess a number between 0 and 20: ")
+        #return guessing_game(g)
+    #g = int(g)
+    if g == number:
+        print("Congratulations! You guessed the number correctly!")
+        return g
+    elif g > number and g <= 20:
+        print("Your guess was too high, please try again!")
+        return g
+    elif g < number and g <= 20:
+        print("Your guess was too low, please try again!")
+        return g
+    else:
+        print("The game cannot continue due to an invalid input or answer. Please make sure your guess is between 0 and 20.")
+        return g
+
+number = random.randint(0,20)
+guess = input("Please guess a number between 0 and 20: ")
+#while True:
+    #if x == "Value Error, please check again!":
+        #print("This is not a number. Try typing in your answer as a number, not a character or string of words: ")
+        #guess = input("Please guess a number between 0 and 20: ")
+    #else:
+        #pass
+while type(guess) != int:
+    guess = num_check(guess)
+answer = guessing_game(guess)
+
+while answer != number:
+    try:
+        print("Your guess was " + str(answer) + ".")
+        guess = input("Please guess a number between 0 and 20: ")
+        while type(guess) != int:
+            guess = num_check(guess)
+        answer = guessing_game(guess)
+    except ValueError:
+        print("Warning! What you typed in was either not a number or contained things that were not numbers. Please make sure you are answering properly with numbers.")
+        guess = input("Please guess a number between 0 and 20: ")
+        while type(guess) != int:
+            guess = num_check(guess)
+        answer = guessing_game(guess)
+#guess = guessing_game(guess)
+#print(type(guess))
